03483261f6f5b22d663a0055257b019b41b46233
RedSut <davidesut@gmail.com>
2022-02-11 16:14:54 +0100

    Fix problema cartelle

---
+++ src/View/MainWindow.h
@@ -0,0 +1,72 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QObject>
+#include <QHBoxLayout>
+#include <QMenu>
+#include <QMenuBar>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QFrame>
+#include <QString>
+#include <QStackedWidget>
+#include <QMessageBox>
+#include <QFileDialog>
+#include "graficoView.h"
+#include "tableView.h"
+#include "../Model/classi/graficoJSON.h"
+
+class Controller;
+
+class MainWindow :public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void setController(Controller *c);
+    void setGrafico(grafico*);
+    void updateGraficoFromTable(grafico*);
+
+    void goToSecondPage();
+    void goToFirstPage();
+
+    void aggiungiRigaTabella(grafico*);
+    void aggiungiColonnaTabella(grafico*);
+    void rimuoviRigaTabella();
+    void rimuoviColonnaTabella();
+    void modificaSezioneVTabella(int,grafico*);
+    void modificaSezioneHTabella(int,grafico*);
+    void updateRegressioneLineare();
+
+    void openFile(graficoJSON*);
+    void saveFileAs(graficoJSON*);
+    void saveFile(graficoJSON*);
+
+private:
+    graficoView* graficoWidget;
+    tableView* graficoTabella;
+    //Ui::MainWindow *ui;
+
+    Controller* controller;
+    QHBoxLayout* buttons;
+    QStackedWidget* pagine;
+
+    QPushButton* pieChart;
+    QPushButton* histogram;
+    QPushButton* barChart;
+    QPushButton* lineChart;
+    QPushButton* dispersionChart;
+
+    QMenuBar* menuBar;
+    QMenu* file;
+
+    QString nomeFileAperto;
+
+    void addMenuBar();
+    QHBoxLayout* addMainButtons();
+
+};
+#endif // MAINWINDOW_H
